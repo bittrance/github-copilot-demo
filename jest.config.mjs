58aceca236d44dd5b61df289e571f0cd13803564
Bittrance <bittrance@gmail.com>
2023-09-21 20:36:55 +0200

    package.json et al.

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  moduleFileExtensions: ["js", "mjs"],
+  testRegex: [".*\.test.*"],
+};
